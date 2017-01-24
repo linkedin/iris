0b9c1863218cd971fb3f9d4598fc98b9c6bcae2a
Qingping Hou <dave2008713@gmail.com>
2017-01-24 10:30:27 -0800

    run tests in travis ci (#2)

---
+++ .ci/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${CI_DIR}/common.sh"
+
+pushd ${TRAVIS_BUILD_DIR}
+make serve &
+iris-sender ./configs/config.dev.yaml &
+
+make test
