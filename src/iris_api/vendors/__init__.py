d12a97319fdff6cc5194a1c1cb84a7945933465d
Joe Gillotti <jgillotti@linkedin.com>
2017-03-15 15:47:05 -0700

    Avoid deepcopy in vendors

--- src/iris_api/vendors/__init__.py
+++ src/iris_api/vendors/__init__.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 import logging
 import itertools
 import random
-import copy
 logger = logging.getLogger(__name__)
 
 _max_tries_per_message = 5
@@ -19,25 +18,26 @@ class IrisVendorException(Exception):
 
 
 def init_vendors(vendors, application_vendors):
+    applications = {application_cls.name: application_cls for application_cls in (import_custom_module('iris_api.applications', application) for application in application_vendors)}
+
     vendor_instances = defaultdict(list)
+    app_vendor_instances = defaultdict(lambda: defaultdict(list))
     for vendor in vendors:
-        instance = import_custom_module('iris_api.vendors', vendor['type'])(vendor)
-        for mode in instance.supports:
-            vendor_instances[mode].append(instance)
+        vendor_cls = import_custom_module('iris_api.vendors', vendor['type'])
+        for mode in vendor_cls.supports:
+            vendor_instances[mode].append(vendor_cls(vendor))
+            for application_name, application_cls in applications.iteritems():
+                app_vendor_instances[application_name][mode].append(application_cls(vendor_cls(vendor)))
 
     for mode, instances in vendor_instances.iteritems():
         random.shuffle(instances)
         _vendors_iter[mode] = itertools.cycle(instances)
 
-    # Create application-specific versions of those vendors
-    for application in application_vendors:
-        application_cls = import_custom_module('iris_api.applications', application)
-        application_name = application_cls.name
-
-        logger.info('Loaded application %s', application_name)
-
-        for mode, instances in vendor_instances.iteritems():
-            _app_specific_vendors_iter[application_name][mode] = itertools.cycle(application_cls(copy.deepcopy(instance)) for instance in instances)
+    for application_name, modes in app_vendor_instances.iteritems():
+        for mode_name, instances in modes.iteritems():
+            random.shuffle(instances)
+            _app_specific_vendors_iter[application_name][mode_name] = itertools.cycle(instances)
+        logger.info('Initialized application %s', application_name)
 
 
 def send_message(message):
