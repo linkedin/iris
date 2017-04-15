0942a6968f98a5f588d225390270c75ba346a9ca
Qingping Hou <dave2008713@gmail.com>
2017-04-14 17:56:51 -0700

    ci: fail early on error in install.sh

--- .ci/common.sh
+++
@@ -1,6 +0,0 @@
-export WORKON_HOME=~/.virtualenvs
-
-[ -e /usr/local/bin/virtualenvwrapper.sh ] && {
-	. /usr/local/bin/virtualenvwrapper.sh;
-	workon iris-api
-}
--- .ci/install.sh
+++ .ci/install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+set -e
+
 CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${CI_DIR}/common.sh"
 
 bash ${CI_DIR}/run_mysql_docker.sh
 
-sudo pip install virtualenvwrapper
-. /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv iris-api
-workon iris-api
-
 pushd ${TRAVIS_BUILD_DIR}
+	echo "[*] installing app dependencies..."
 	python setup.py develop
+	echo "[*] pip installing dev_requirements.txt..."
 	pip install -r dev_requirements.txt
 popd
 
 bash ${CI_DIR}/setup_mysql.sh
+
+set +e
