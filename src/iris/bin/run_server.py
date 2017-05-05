325dde3ec4ba10ef01510132fc387305b44478e6
Qingping Hou <dave2008713@gmail.com>
2017-05-05 13:01:12 -0700

    use gunicorn for run_server

--- src/iris/bin/run_server.py
+++ src/iris/bin/run_server.py
@@ -3,83 +3,42 @@
 # Copyright (c) LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
 # See LICENSE in the project root for license information.
 
-# -*- coding:utf-8 -*-
-
-import gevent
-from gevent import monkey
-monkey.patch_all()  # noqa
-from gevent.pywsgi import WSGIServer
-from gevent.subprocess import call
-
-import time
-import fnmatch
-import signal
 import sys
-import os
-
-import logging
-logging.basicConfig()
-
-try:
-    import gevent_inotifyx as inotify
-except ImportError:
-    HAS_INOTIFY = False
-else:
-    HAS_INOTIFY = True
+import multiprocessing
+import gunicorn.app.base
+from gunicorn.six import iteritems
+from iris.config import load_config
+from iris.api import get_api
 
 
-def event_producer(fd, server):
-    while True:
-        restart = False
-        events = inotify.get_events(fd, None)
-        for event in events:
-            ignore = False
-            if call(['git', 'check-ignore', event.name]) == 0 or fnmatch.fnmatch(event.name, '.git/*'):
-                ignore = True
-                break
-            if not ignore:
-                print "File changed:", event.name, event.get_mask_description()
-                restart = True
-        if restart:
-            print 'Restarting %s ...\n' % sys.argv
-            server.stop(timeout=1)
-            server.close()
-            print 'Waiting...'
-            time.sleep(3)
-            print 'Restart.'
-            os.execvp(sys.argv[0], sys.argv)
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
 
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super(StandaloneApplication, self).__init__()
 
-def sigint_handler(sig, frame):
-    print 'Caught sigint. Quitting.'
-    sys.exit(0)
+    def load_config(self):
+        config = {key: value for key, value in iteritems(self.options)
+                  if key in self.cfg.settings and value is not None}
+        for key, value in iteritems(config):
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
 
 
 def main():
-    signal.signal(signal.SIGINT, sigint_handler)
-
-    from iris.api import get_api, load_config
-
-    config = load_config()
-    app = get_api(config)
-
+    config = load_config(sys.argv[1])
     server = config['server']
-    print 'LISTENING: %(host)s:%(port)d' % server
-    server = WSGIServer((server['host'], server['port']), app)
 
-    if HAS_INOTIFY:
-        fd = inotify.init()
+    options = {
+        'reload': True,
+        'bind': '%s:%s' % (server['host'], server['port']),
+        'worker_class': 'gevent',
+        'accesslog': '-',
+        'workers': (multiprocessing.cpu_count() * 2) + 1
+    }
 
-        for dirname, subfolders, _ in os.walk('.'):
-            if '.git' in subfolders:
-                subfolders.remove('.git')
-            inotify.add_watch(fd, dirname, inotify.IN_MODIFY)
-
-        gevent.spawn(event_producer, fd, server)
-    else:
-        print 'Missing inotify, disable watch support.'
-    server.serve_forever()
-
-
-if __name__ == '__main__':
-    main()
+    gunicorn_server = StandaloneApplication(get_api(config), options)
+    gunicorn_server.run()
