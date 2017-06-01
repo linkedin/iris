5b4b1d8df9de43e845cf4350aa51324e723f6bb0
Qingping Hou <qihou@linkedin.com>
2017-05-31 21:33:27 -0700

    test: fix e2e coverage report
    SIGINT doesn't always reliably kill the test server

--- test/e2etest_coverage.sh
+++ test/e2etest_coverage.sh
@@ -4,16 +4,16 @@ cd $(dirname ${CURDIR})
 if [ ! -z "$SUPPORT_COMBINED_COVERAGE" ]; then
   export COVERAGE_FILE=.coverage.e2e
 fi
-coverage run --source=iris ./test/e2eserver.py configs/config.dev.yaml &>/dev/null &
+coverage run --concurrency=gevent --source=iris ./test/e2eserver.py configs/config.dev.yaml &>/dev/null &
 pid=$!
 sleep 2
-if ! kill -0 "$pid" ; then
+if ! /bin/kill -0 "$pid" ; then
   echo Server failed to start. Bailing.
   exit 1
 fi
-py.test -v test/e2etest.py
+py.test test/e2etest.py
 sleep 1
-/bin/kill -INT $pid
+/bin/kill -TERM $pid
 if [ -z "$SUPPORT_COMBINED_COVERAGE" ]; then
   coverage report -m
 fi
