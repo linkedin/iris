3e2fb9dc7f6fb993261009e24bbbafbe259c93f6
Qingping Hou <qihou@linkedin.com>
2017-06-15 18:39:03 -0700

    add tests for smtp vendor

---
+++ test/test_iris_vendor_smtp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+def test_smtp_vendor_init():
+    from iris.vendors.iris_smtp import iris_smtp
+    smtp_config = {
+        'smtp_server': 'foo'
+    }
+    smtp_vendor = iris_smtp(smtp_config)
+    assert smtp_vendor.smtp_timeout == 10
+    assert smtp_vendor.mx_sorted == [(0, 'foo')]
+
+    smtp_config = {
+        'timeout': 20,
+        'smtp_server': 'foo'
+    }
+    smtp_vendor = iris_smtp(smtp_config)
+    assert smtp_vendor.smtp_timeout == 20
+
+
+def test_smtp_send_email(mocker):
+    from iris.vendors.iris_smtp import iris_smtp
+    smtp_config = {
+        'smtp_server': 'foo',
+        'from': 'iris@bar',
+    }
+    smtp_vendor = iris_smtp(smtp_config)
+    mocked_SMPT = mocker.patch('iris.vendors.iris_smtp.SMTP')
+    smtp_vendor.send_email({
+        'destination': 'foo@bar',
+        'subject': 'hello',
+        'body': 'world',
+    })
+    mocked_SMPT.assert_called_once_with(timeout=10)
+
+    class CheckEmailString(str):
+        def __eq__(self, other):
+            return 'hello' in other and 'world' in other
+
+    mocked_SMPT.return_value.sendmail.assert_called_once_with(
+        ['iris@bar'],
+        ['foo@bar'],
+        CheckEmailString())
+    mocked_SMPT.return_value.quit.assert_called_once_with()
