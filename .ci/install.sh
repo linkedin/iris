14f0325f7cbd31c56d743a8af8d77bda3f7c4049
Daniel Wang(Application Operations) <dewang@dewang-ld2.linkedin.biz>
2017-12-01 12:57:54 -0800

    apt-get update before install

--- .ci/install.sh
+++ .ci/install.sh
@@ -7,6 +7,7 @@ source "${CI_DIR}/common.sh"
 bash ${CI_DIR}/run_mysql_docker.sh
 
 echo "[*] installing app dependencies..."
+sudo apt-get update
 sudo apt-get install libsasl2-dev python-dev libldap2-dev libssl-dev  # for ldap
 pip install -e '.[dev,kazoo]'
 
