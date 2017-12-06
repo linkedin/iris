b933ba2f2fdef267bffc18bbbecc08710c192eeb
Joe Gillotti <jgillotti@linkedin.com>
2017-12-06 12:27:09 -0800

    Validate username + show login errors on login form
    - Add "flashing", where we add a single message/alert to the session and it
      then persists, and gets removed when the page is loaded again
    
    - When you try to login with an invalid username, say so if we're in debug
      mode, but give a vague error if we're in prod mode, to prevent people
      guessing valid usernames
    
    - When you try to login with bad credentials, say that instead of refreshing
      the page and not saying anything.
    
    Addresses https://github.com/linkedin/iris/issues/309

--- src/iris/ui/auth/__init__.py
+++ src/iris/ui/auth/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
 # See LICENSE in the project root for license information.
 
+from iris import db
+
 
 def login_user(req, username):
     session = req.env['beaker.session']
@@ -12,3 +14,17 @@ def logout_user(req):
     session = req.env['beaker.session']
     session.pop('user', None)
     session.delete()
+
+
+def valid_username(username):
+    connection = db.engine.raw_connection()
+    cursor = connection.cursor()
+    cursor.execute('''
+    SELECT EXISTS(SELECT 1
+                  FROM `target`
+                  JOIN `user` on `target`.`id` = `user`.`target_id`
+                  WHERE `name` = %s AND `type_id` = (SELECT `id` FROM `target_type` WHERE `name` = "user"))''', username)
+    result = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    return result[0] == 1
