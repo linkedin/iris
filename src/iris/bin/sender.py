09d3bd95de22eefe4eceed77525015d561fe997c
William Leese <wleese@bol.com>
2018-01-24 09:57:20 +0100

    dont reuse gwatch_renewer_task
    Change-Id: I65382085d1d57f8919dbd4ce5bf61d422347dc0b

--- src/iris/bin/sender.py
+++ src/iris/bin/sender.py
@@ -1504,7 +1504,8 @@ def main():
 
     maintain_workers(config)
 
-    gwatch_renewer_task = config['sender'].get('disable_gwatch_renewer_task', False)
+    disable_gwatch_renewer = config['sender'].get('disable_gwatch_renewer_task', False)
+    gwatch_renewer_task = None
     prune_audit_logs_task = None
 
     interval = 60
@@ -1528,7 +1529,7 @@ def main():
         # If we're currently a master, ensure our master-greenlets are running
         # and we're doing the master duties
         if coordinator.am_i_master():
-            if not bool(gwatch_renewer_task):
+            if not bool(gwatch_renewer_task) and not disable_gwatch_renewer:
                 if should_mock_gwatch_renewer:
                     gwatch_renewer_task = spawn(mock_gwatch_renewer)
                 else:
