de66538f0fd90b5c66d815acdfd1ffa539559a1a
Patrick Baxter <pb@coreos.com>
2018-06-01 15:03:19 -0700

    Plumb plan name through URL parameter for rackspace webhook

--- src/iris/webhooks/rackspace.py
+++ src/iris/webhooks/rackspace.py
@@ -10,45 +10,43 @@ from iris import db
 logger = logging.getLogger(__name__)
 
 
-class alertmanager(object):
+class rackspace(object):
     allow_read_no_auth = False
 
     def validate_post(self, body):
-        if not all(k in body for k in("version", "status", "alerts")):
-            raise HTTPBadRequest('missing version, status and/or alert attributes')
+        if not all(k in body for k in("event_id", "details")):
+            raise HTTPBadRequest('missing event_id and/or details attributes')
 
-        if 'iris_plan' not in body["groupLabels"]:
-            raise HTTPBadRequest('missing iris_plan in group labels')
 
     def create_context(self, body):
         context_json_str = ujson.dumps(body)
         if len(context_json_str) > 65535:
-            logger.warn('POST to alertmanager exceeded acceptable size')
+            logger.warn('POST to rackspace exceeded acceptable size')
             raise HTTPBadRequest('Context too long')
 
         return context_json_str
 
     def on_post(self, req, resp):
         '''
-        This endpoint is compatible with the webhook post from Alertmanager.
-        Simply configure alertmanager with a receiver pointing to iris, like
-        so:
+        This endpoint is compatible with the webhook posts from Rackspace.
+        Configure a Rackspace notification to post to a URL with the following
+        parameters
 
-        receivers:
-        - name: 'iris-team1'
-          webhook_configs:
-            - url: http://iris:16649/v0/webhooks/alertmanager?application=test-app&key=sdffdssdf
+        "http://iris:16649/v0/webhooks/rackspace?application=test-app&key=abc&plan=teamA"
 
         Where application points to an application and key in Iris.
 
-        For every POST from alertmanager, a new incident will be created, if the iris_plan label
+        For every POST from Rackspace, a new incident will be created, if the plan label
         is attached to an alert.
         '''
         alert_params = ujson.loads(req.context['body'])
         self.validate_post(alert_params)
 
         with db.guarded_session() as session:
-            plan = alert_params['groupLabels']['iris_plan']
+            plan = req.context['plan']
+            if plan is None:
+                raise HTTPBadRequest('missing plan in url parameters')
+
             plan_id = session.execute('SELECT `plan_id` FROM `plan_active` WHERE `name` = :plan',
                                       {'plan': plan}).scalar()
             if not plan_id:
