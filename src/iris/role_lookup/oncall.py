c083a0418e4c2c5896e71c7ba34d25691a276e16
Daniel Wang <dewang@linkedin.com>
2018-12-04 14:11:22 -0800

    Change role target lookup error to ignore misconfigured plan
    Role target lookup errors should only track when it's Iris's fault,
    not user error for misconfigured plans

--- src/iris/role_lookup/oncall.py
+++ src/iris/role_lookup/oncall.py
@@ -5,6 +5,7 @@ import requests
 import logging
 from iris.metrics import stats
 import oncallclient
+from iris.role_lookup import IrisRoleLookupException
 
 logger = logging.getLogger(__name__)
 
@@ -27,25 +28,37 @@ class oncall(object):
         try:
             r = self.requests.get(url)
         except Exception:
-            logger.exception('Failed hitting oncall-api for url "%s"', url)
-            return None
+            stats['oncall_error'] += 1
+            msg = 'Failed hitting oncall-api for url "%s"' % url
+            logger.exception(msg)
+            raise IrisRoleLookupException(msg)
 
         if r.status_code != 200:
-            logger.error('Invalid response from oncall-api for URL "%s". Code: %s. Content: "%s"', url, r.status_code, r.content)
-            return None
+            # 422 from Oncall indicates nonexistent team from a misconfigured plan; return None for this case
+            if r.status_code == 422:
+                logger.warning('422 for url "%s", likely invalid plan')
+                return None
+            else:
+                stats['oncall_error'] += 1
+                msg = 'Invalid response from oncall-api for URL "%s". Code: %s. Content: "%s"' \
+                    % (url, r.status_code, r.content)
+                logger.error(msg)
+                raise IrisRoleLookupException(msg)
 
         try:
             return r.json()
         except ValueError:
-            logger.exception('Failed decoding json from oncall-api. URL: "%s" Code: %s', url, r.status_code)
-            return None
+            stats['oncall_error'] += 1
+            msg = 'Failed decoding json from oncall-api. URL: "%s" Code: %s' % (url, r.status_code)
+            logger.exception(msg)
+            raise IrisRoleLookupException(msg)
 
     def get(self, role, target):
         if role == 'team':
             result = self.call_oncall('/teams/%s/rosters' % target)
             if not isinstance(result, dict):
                 stats['oncall_error'] += 1
-                return None
+                raise IrisRoleLookupException('Invalid data received from oncall-api')
             user_list = []
             for roster in result:
                 for user in result[roster]['users']:
@@ -55,14 +68,18 @@ class oncall(object):
             result = self.call_oncall('/teams/%s/oncall/manager' % target)
             if not isinstance(result, list):
                 stats['oncall_error'] += 1
-                return None
+                raise IrisRoleLookupException('Invalid data received from oncall-api')
             if result:
                 return [user['user'] for user in result]
+        # "oncall" role maps to primary, otherwise grab shift type from the role suffix (eg "oncall-shadow")
         elif role.startswith('oncall'):
             oncall_type = 'primary' if role == 'oncall' else role[7:]
             result = self.call_oncall('/teams/%s/oncall/%s' % (target, oncall_type))
             if not isinstance(result, list):
                 stats['oncall_error'] += 1
-                return None
+                raise IrisRoleLookupException('Invalid data received from oncall-api')
             if result:
                 return [user['user'] for user in result]
+        # Other roles can't be handled by this plugin; return None.
+        else:
+            return None
