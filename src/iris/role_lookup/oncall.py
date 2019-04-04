3f1f5b5c0a5d99b1c929af98df13dab0d9642209
Daniel Wang <dewang@linkedin.com>
2019-04-03 18:24:11 -0700

    Fix oncall error metrics

--- src/iris/role_lookup/oncall.py
+++ src/iris/role_lookup/oncall.py
@@ -23,7 +23,7 @@ class oncall(object):
         self.requests.headers = headers
         self.endpoint = config['oncall-api'] + '/api/v0'
 
-    def call_oncall(self, url):
+    def call_oncall(self, url, expected_type):
         url = str(self.endpoint + url)
         try:
             r = self.requests.get(url)
@@ -36,7 +36,7 @@ class oncall(object):
         if r.status_code != 200:
             # 422 from Oncall indicates nonexistent team from a misconfigured plan; return None for this case
             if r.status_code == 422:
-                logger.warning('422 for url "%s", likely invalid plan')
+                logger.warning('422 for url "%s", likely invalid plan' % url)
                 return None
             else:
                 stats['oncall_error'] += 1
@@ -46,7 +46,13 @@ class oncall(object):
                 raise IrisRoleLookupException(msg)
 
         try:
-            return r.json()
+            data = r.json()
+            if not isinstance(data, expected_type):
+                stats['oncall_error'] += 1
+                msg = 'Invalid data recieved from oncall-api for URL %s' % url
+                logger.error(msg)
+                raise IrisRoleLookupException(msg)
+            return data
         except ValueError:
             stats['oncall_error'] += 1
             msg = 'Failed decoding json from oncall-api. URL: "%s" Code: %s' % (url, r.status_code)
@@ -55,31 +61,29 @@ class oncall(object):
 
     def get(self, role, target):
         if role == 'team':
-            result = self.call_oncall('/teams/%s/rosters' % target)
-            if not isinstance(result, dict):
-                stats['oncall_error'] += 1
-                raise IrisRoleLookupException('Invalid data received from oncall-api')
+            result = self.call_oncall('/teams/%s/rosters' % target, dict)
             user_list = []
-            for roster in result:
-                for user in result[roster]['users']:
-                    user_list.append(user['name'])
-            return user_list
+            if result:
+                for roster in result:
+                    for user in result[roster]['users']:
+                        user_list.append(user['name'])
+                return user_list
+            else:
+                return None
         elif role == 'manager':
-            result = self.call_oncall('/teams/%s/oncall/manager' % target)
-            if not isinstance(result, list):
-                stats['oncall_error'] += 1
-                raise IrisRoleLookupException('Invalid data received from oncall-api')
+            result = self.call_oncall('/teams/%s/oncall/manager' % target, list)
             if result:
                 return [user['user'] for user in result]
+            else:
+                return None
         # "oncall" role maps to primary, otherwise grab shift type from the role suffix (eg "oncall-shadow")
         elif role.startswith('oncall'):
             oncall_type = 'primary' if role == 'oncall' else role[7:]
-            result = self.call_oncall('/teams/%s/oncall/%s' % (target, oncall_type))
-            if not isinstance(result, list):
-                stats['oncall_error'] += 1
-                raise IrisRoleLookupException('Invalid data received from oncall-api')
+            result = self.call_oncall('/teams/%s/oncall/%s' % (target, oncall_type), list)
             if result:
                 return [user['user'] for user in result]
+            else:
+                return None
         # Other roles can't be handled by this plugin; return None.
         else:
             return None
