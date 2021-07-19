81a6b573012afa32a366e7a78722bb31a0c559ae
Patrick Baxter <patrickbx@gmail.com>
2021-07-19 10:37:26 -0700

    rackspace webhook support (#405)
    * Start rackspace webhook impl by copying alertmanager
    
    * Plumb plan name through URL parameter for rackspace webhook
    
    * add test for rackspace webhook (WIP)
    
    * fix rackspace webhook test data
    
    * rackspace webhook should parse plan inside its class
    
    This detail is only relevant to the rackspace webhook class and doesn't belong
    in api.py. This just reverts the changes in api.py and adds support for parsing
    the plan in the rackspace webhook class.
    
    * deduplicate webhook code with class inheritance
    
    authored-by: Patrick Baxter <pb@coreos.com>

--- src/iris/webhooks/alertmanager.py
+++ src/iris/webhooks/alertmanager.py
@@ -1,15 +1,12 @@
-import datetime
-import logging
+from __future__ import absolute_import
+
 import ujson
-from falcon import HTTP_201, HTTPBadRequest, HTTPNotFound
+from falcon import HTTPBadRequest
 
-from iris import db
-
-logger = logging.getLogger(__name__)
+from iris.webhooks.webhook import webhook
 
 
-class alertmanager(object):
-    allow_read_no_auth = False
+class alertmanager(webhook):
 
     def validate_post(self, body):
         if not all(k in body for k in("version", "status", "alerts")):
@@ -17,14 +14,6 @@ class alertmanager(object):
 
         if 'iris_plan' not in body["groupLabels"]:
             raise HTTPBadRequest('missing iris_plan in group labels')
-
-    def create_context(self, body):
-        context_json_str = ujson.dumps(body)
-        if len(context_json_str) > 65535:
-            logger.warning('POST to alertmanager exceeded acceptable size')
-            raise HTTPBadRequest('Context too long')
-
-        return context_json_str
 
     def on_post(self, req, resp):
         '''
@@ -43,55 +32,5 @@ class alertmanager(object):
         is attached to an alert.
         '''
         alert_params = ujson.loads(req.context['body'])
-        self.validate_post(alert_params)
-
-        with db.guarded_session() as session:
-            plan = alert_params['groupLabels']['iris_plan']
-            plan_id = session.execute('SELECT `plan_id` FROM `plan_active` WHERE `name` = :plan',
-                                      {'plan': plan}).scalar()
-            if not plan_id:
-                raise HTTPNotFound()
-
-            app = req.context['app']
-
-            if not session.execute('SELECT EXISTS(SELECT 1 FROM `application` WHERE id = :id)', {'id': app['id']}).scalar():
-                raise HTTPBadRequest('Invalid application')
-
-            context_json_str = self.create_context(alert_params)
-
-            app_template_count = session.execute('''
-                SELECT EXISTS (
-                  SELECT 1 FROM
-                  `plan_notification`
-                  JOIN `template` ON `template`.`name` = `plan_notification`.`template`
-                  JOIN `template_content` ON `template_content`.`template_id` = `template`.`id`
-                  WHERE `plan_notification`.`plan_id` = :plan_id
-                  AND `template_content`.`application_id` = :app_id
-                )
-            ''', {'app_id': app['id'], 'plan_id': plan_id}).scalar()
-
-            if not app_template_count:
-                logger.warning('no plan template exists for this app')
-                raise HTTPBadRequest('No plan template actions exist for this app')
-
-            data = {
-                'plan_id': plan_id,
-                'created': datetime.datetime.utcnow(),
-                'application_id': app['id'],
-                'context': context_json_str,
-                'current_step': 0,
-                'active': True,
-            }
-
-            incident_id = session.execute(
-                '''INSERT INTO `incident` (`plan_id`, `created`, `context`,
-                                           `current_step`, `active`, `application_id`)
-                   VALUES (:plan_id, :created, :context, 0, :active, :application_id)''',
-                data).lastrowid
-
-            session.commit()
-            session.close()
-
-        resp.status = HTTP_201
-        resp.set_header('Location', '/incidents/%s' % incident_id)
-        resp.body = ujson.dumps(incident_id)
+        plan = alert_params['groupLabels']['iris_plan']
+        super().on_post(self, req, resp, plan)
