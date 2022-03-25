21d6ee95c750b4bb8b7657d7bd9da9bad82559cf
allwyn-pradip <allwyn.pradip@freshworks.com>
2022-03-25 13:02:42 +0530

    spell check

--- src/iris/api.py
+++ src/iris/api.py
@@ -1397,14 +1397,14 @@ class Plans(object):
 
             for index, steps in enumerate(plan_params['steps'], start=1):
 
-                # A plan must have at least one non-optonal notification per step, if it doesn't reject the plan
+                # A plan must have at least one non-optional notification per step, if it doesn't reject the plan
                 only_optional_flag = True
 
                 for step in steps:
                     dynamic = step.get('dynamic_index') is not None
                     step['plan_id'] = plan_id
                     step['step'] = index
-                    # for backwards copatibility check if optional is not defined and set it to 0 if it isn't
+                    # for backwards compatibility check if optional is not defined and set it to 0 if it isn't
                     step.setdefault('optional', 0)
                     if step['optional'] == 0:
                         only_optional_flag = False
