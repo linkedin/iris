5a15d3e1b930321948279adb47f2ef049cb942db
Diego Cepeda <dcepeda@linkedin.com>
2024-06-11 12:41:01 -0600

    update test

--- test/e2etest.py
+++ test/e2etest.py
@@ -1541,6 +1541,7 @@ def test_post_dynamic_incident(sample_user, sample_team, sample_application_name
     incident_response = re.json()
     incident_response.pop('created')
     incident_response.pop('id')
+    incident_response['dynamic_tracking'].pop('incident_id')
     assert incident_response == incident_data
 
     # Claim
