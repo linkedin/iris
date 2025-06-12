cb83937d45bc2ab652bcf8a0b202ea2920ab8f0c
Diego Cepeda <dcepeda@linkedin.com>
2025-06-12 11:12:45 -0500

    retry plan insertion (#782)
    * restricted plans (#17)
    
    * [DB SCHEMA CHANGE] admin restricted plans
    
    * flake8
    
    * update test
    
    * test
    
    * test
    
    * modify test
    
    * add exists search operator for tags
    
    * flake8
    
    * consolidate tag where functions
    
    * use extent() instead of loop
    
    * allow sorting incident results by custom fields
    
    * trigger-tests
    
    * tweak default
    
    * trigger-tests
    
    * update test
    
    * change test
    
    * tweak test
    
    * fix result json dump
    
    * tweak
    
    * fix
    
    * tweak test
    
    * tweak
    
    * modicy test
    
    * update test
    
    * change order_by default
    
    * rename format_incidents_with_context
    
    * don't reject incidents with too many dynamic targets
    
    * test
    
    * test
    
    * change error message
    
    * update sphinx set-up
    
    * format file
    
    * allow using ssl for db connection
    
    * support DB with ONLY_FULL_GROUP_BY on
    
    * fix template where
    
    * refactor count queries into smaller sub functions
    
    * re-add docstring
    
    * re add doc-string
    
    * add safe retries fuction and use it for plans
    
    * repeat change

--- src/iris/api.py
+++ src/iris/api.py
@@ -36,7 +36,7 @@ from iris.sender import auditlog
 from iris.sender.quota import (get_application_quotas_query,
                                insert_application_quota_query, quota_int_keys,
                                required_quota_keys)
-from iris.utils import sanitize_unicode_dict
+from iris.utils import sanitize_unicode_dict, safe_execute_with_retries, safe_commit_with_retries
 from iris.vendors.iris_slack import iris_slack
 
 from . import app_stats, cache, client, db, ui, utils, sso
@@ -1736,8 +1736,9 @@ class Plans(object):
 
                 if 'dynamic_index' in step:
                     dynamic_indices.add(step['dynamic_index'])
-                if (step.get('wait', 0) * step.get('count', 0)) > longest_step:
-                    longest_step = step.get('wait', 0) * step.get('count', 0)
+                message_count = step.get('repeat', 0) + 1
+                if (step.get('wait', 0) * message_count) > longest_step:
+                    longest_step = step.get('wait', 0) * message_count
             plan_length += longest_step
 
         if dynamic_indices != set(range(len(dynamic_indices))):
@@ -1762,7 +1763,8 @@ class Plans(object):
                     % plan_dict["name"],
                 )
             try:
-                plan_id = session.execute(text(insert_plan_query), plan_dict).lastrowid
+                result = safe_execute_with_retries(session, text(insert_plan_query), plan_dict)
+                plan_id = result.lastrowid
             except IntegrityError:
                 session.rollback()
                 session.close()
@@ -1813,9 +1815,9 @@ class Plans(object):
 
                     try:
                         if dynamic:
-                            session.execute(text(insert_dynamic_step_query), step)
+                            safe_execute_with_retries(session, text(insert_dynamic_step_query), step)
                         else:
-                            session.execute(text(insert_plan_step_query), step)
+                            safe_execute_with_retries(session, text(insert_plan_step_query), step)
                     except IntegrityError:
                         raise HTTPBadRequest('Invalid plan',
                                              'Invalid data for step %s' % index)
@@ -1825,9 +1827,10 @@ class Plans(object):
 
             query = '''INSERT INTO `plan_active` (`name`, `plan_id`)
                        VALUES (:name, :plan_id) ON DUPLICATE KEY UPDATE `plan_id`=:plan_id'''
-            session.execute(text(query), {'name': plan_name, 'plan_id': plan_id})
+            params = {'name': plan_name, 'plan_id': plan_id}
+            safe_execute_with_retries(session, text(query), params)
 
-            session.commit()
+            safe_commit_with_retries(session)
             session.close()
         resp.status = HTTP_201
         resp.body = ujson.dumps(plan_id)
